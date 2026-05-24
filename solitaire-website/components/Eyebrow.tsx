cd3c822c5b49b75d1142a10fd18b32994d6a8e3b
Vaibhav Tiwari <tiwarvaibhav997@gmail.com>
2026-05-24 17:30:13 +0530

    clean up repo structure

--- solitaire-website/components/Eyebrow.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-  className?: string;
-  as?: 'p' | 'span';
-};
-
-export function Eyebrow({ children, className = '', as: Tag = 'p' }: Props) {
-  return <Tag className={`eyebrow ${className}`}>{children}</Tag>;
-}
